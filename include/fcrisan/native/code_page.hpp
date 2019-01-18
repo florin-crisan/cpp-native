e2d4e1e081e3c346b1d94545b5973702de5ad971
Florin Crișan <florin.crisan@gmail.com>
2019-01-18 17:10:41 +0200

    Added symbolic names for all code pages used by the tests

--- include/fcrisan/native/code_page.hpp
+++ include/fcrisan/native/code_page.hpp
@@ -10,8 +10,21 @@ namespace fcrisan::native {
 	typedef unsigned int code_page_id;
 
 	namespace code_pages {
-		static const code_page_id utf8 = /*CP_UTF8*/ 65001;
-		static const code_page_id utf16 = 1200; // Not actually a system code page, just an ID that we (and .NET) use.
+		const code_page_id ascii = 20127;
+		const code_page_id utf8 = 65001;
+		const code_page_id utf16_le = 1200; // Not actually a system code page, just an ID that we (and .NET) use.
+		const code_page_id utf16_be = 1201; // Not actually a system code page, just an ID that we (and .NET) use.
+		const code_page_id dos_us = 437;
+		const code_page_id dos_western_europe = 850;
+		const code_page_id dos_eastern_europe = 852;
+		const code_page_id windows_western_europe = 1252;
+		const code_page_id windows_eastern_europe = 1250;
+
+#   if defined(_M_IX86) || defined(_M_X64)
+		const code_page_id utf16 = utf16_le;
+#   else
+#       error Todo: figure out if machine is little endian or big endian.
+#   endif
 	}
 #else
 #   error Not implemented for this platform.
